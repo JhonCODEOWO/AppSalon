e59ce823966f6b9cecbccd3aad824e8313c3d8b0
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-08-25 12:15:01 -0600

    refactor: Implements new main classes to style some elements.

--- views/Auth/login.php
+++ views/Auth/login.php
@@ -1,4 +1,33 @@
-<form action="" method="post" class="text-center">
+<form action="/login" method="post" class="text-center">
     <h1 class="text-center">Iniciar sesión</h1>
     <p>Coloca tus credenciales para iniciar sesión.</p>
+
+    <fieldset class="input-fieldset">
+        <legend for="email">Email</legend>
+        <input 
+            type="email" 
+            name="email" 
+            id="email"
+            placeholder="Escribe el correo de tu cuenta"
+        >
+    </fieldset>
+    <fieldset class="input-fieldset">
+        <legend for="password">Contraseña</legend>
+        <input 
+            type="password" 
+            name="password" 
+            id="password"
+            placeholder="Tu contraseña"
+        >
+    </fieldset>
+    <button 
+        type="submit"
+        class="button button-info"
+    >
+        Iniciar sesión
+    </button>
+    <div class="flex justify-between w-full mt-3">
+        <a href="/forgot-password">¿Olvidaste tu contraseña?</a>
+        <a href="/create-account">Crear cuenta</a>
+    </div>
 </form>
