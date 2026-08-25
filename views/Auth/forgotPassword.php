754bb9279624708976894db96d490cc80f68d9d7
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-08-25 13:00:17 -0600

    refactor: Adds fields to main form.

--- views/Auth/forgotPassword.php
+++ views/Auth/forgotPassword.php
@@ -1,3 +1,15 @@
-<form action="" method="post">
+<form action="/forgotPassword" method="post">
     <h1 class="text-center">Recuperar contraseña</h1>
+    <p>Escribe tu correo, nosotros nos encargaremos de enviarte un email con los siguientes pasos.</p>
+
+    <fieldset class="input-fieldset">
+        <legend for="email">Email</legend>
+        <input type="email" name="email" id="email" placeholder="Escribe el email de la cuenta a recuperar.">
+    </fieldset>
+
+    <button type="submit" class="button button-warning">Buscar y recuperar cuenta.</button>
 </form>
+
+<p class="mt-5">
+    ¿No es lo que deseabas hacer? <a href="/login" class="font-bold">inicia sesión</a>.
+</p>
