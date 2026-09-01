d15b9d5d244893b8dc6a66a1c9dfb727d7071bc1
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-01 14:37:54 -0600

    feat: Adds view to show info about a new account steps.

---
+++ views/Auth/accountCreatedConfirm.php
@@ -0,0 +1,4 @@
+<h1>Tu cuenta se ha creado correctamente</h1>
+<p>Hemos enviado un correo electrónico de confirmación al correo electrónico que ingresaste.</p>
+
+<p>Confirma tu cuenta verificando el correo que te hemos enviado e <a href="/login">inicia sesión</a> después de confirmarla.</p>
