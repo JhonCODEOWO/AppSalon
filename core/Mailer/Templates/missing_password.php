f1368c08eb59fa7b3fab2958c4e9af3fd5d19d19
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-08 14:43:02 -0600

    feat: adds reset password mail template.

---
+++ core/Mailer/Templates/missing_password.php
@@ -0,0 +1,10 @@
+<h1>Restauración de contraseña</h1>
+<p>Has recibido este correo porque se ha solicitado un restablecimiento de contraseña</p>
+
+<strong>Si no has sido tú ignora este correo</strong>
+
+<p>En cambio si lo has solicitado pulsa en el siguiente botón para restablecerla.</p>
+
+<a href="http://<?php echo "$host/reset-password/$token" ?>">
+    Restablecer contraseña
+</a>
