bc176a301f103fa9f922dc246d2dcc4b34539bc8
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-08 14:43:45 -0600

    refactor: Renders a success message if all went ok.

--- views/Auth/forgotPassword.php
+++ views/Auth/forgotPassword.php
@@ -2,9 +2,16 @@
     <h1 class="text-center">Recuperar contraseña</h1>
     <p>Escribe tu correo, nosotros nos encargaremos de enviarte un email con los siguientes pasos.</p>
 
+    <?php if(isset($success)): ?>
+        <p class="success"><?php echo $success ?></p>
+    <?php endif?>
+
     <fieldset class="input-fieldset">
         <legend for="email">Email</legend>
         <input type="email" name="email" id="email" placeholder="Escribe el email de la cuenta a recuperar.">
+        <?php if(isset($errors)): ?>
+            <p class="error"><?php echo $errors->getFrom('email') ?></p>
+        <?php endif ?>
     </fieldset>
 
     <button type="submit" class="button button-warning">Buscar y recuperar cuenta.</button>
