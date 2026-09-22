c041ce1900ddd9fea19e51bb6bc6d668955d1d4d
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-22 11:51:11 -0600

    refactor: Implements $successMessage variable value to show flash data related and mail-status error if its present.

--- views/Auth/forgotPassword.php
+++ views/Auth/forgotPassword.php
@@ -2,15 +2,19 @@
     <h1 class="text-center">Recuperar contraseña</h1>
     <p>Escribe tu correo, nosotros nos encargaremos de enviarte un email con los siguientes pasos.</p>
 
-    <?php if(isset($success)): ?>
-        <p class="success"><?php echo $success ?></p>
-    <?php endif?>
+    <?php if($successMessage != null): ?>
+        <p class="success alert"><?php echo $successMessage ?></p>
+    <?php endif ?>
+    
+    <?php if(error('mail-status') != null): ?>
+        <p class="error alert"><?php echo error('mail-status') ?></p>
+    <?php endif ?>
 
     <fieldset class="input-fieldset">
         <legend for="email">Email</legend>
         <input type="email" name="email" id="email" placeholder="Escribe el email de la cuenta a recuperar.">
-        <?php if(isset($errors)): ?>
-            <p class="error"><?php echo $errors->getFrom('email') ?></p>
+        <?php if(error('email') != null): ?>
+            <p class="error"><?php echo error('email') ?></p>
         <?php endif ?>
     </fieldset>
 
