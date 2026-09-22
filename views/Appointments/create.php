db97e4efe93a68c88156c6b14158b5fe1be9a322
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-22 15:29:15 -0600

    feat: Adds script to navigate into steps using next and previous buttons.

--- views/Appointments/create.php
+++ views/Appointments/create.php
@@ -4,13 +4,13 @@
 
 <div id="steps">
     <nav class="tabs">
-        <button type="button" data-step="0">
+        <button type="button" data-step="1" class="tab">
             Servicios
         </button>
-        <button type="button" data-step="1">
+        <button type="button" data-step="2" class="tab">
             Datos de la cita
         </button>
-        <button type="button" data-step="2">
+        <button type="button" data-step="3" class="tab">
             Confirmación
         </button>
     </nav>
@@ -63,4 +63,69 @@
         <h2>Resumen.</h2>
         <p>Verifica tus datos y finaliza.</p>
     </div>
+
+    <nav class="navigation">
+        <button id="next" class="button button-info">
+            Siguiente
+        </button>
+        <button id="previous" class="button button-info">
+            Anterior
+        </button>
+    </nav>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        //Get every HTML element necessary
+        const step1 = document.querySelector('#step-1');
+        const step2 = document.querySelector('#step-2');
+        const step3 = document.querySelector('#step-3');
+        const tabs = document.querySelectorAll('.tab');
+        const next = document.querySelector('#next');
+        const prev = document.querySelector('#previous');
+
+        //Global variables to manage
+        const steps = [step1, step2, step3];
+        const totalSteps = tabs.length-1;
+        let tabIndex = 0;
+
+        //Main method to manage every step and styles...
+        manageStep(steps, tabs,tabIndex);
+
+        next.addEventListener('click', () => {
+            if(tabIndex == totalSteps) return;
+            tabIndex += 1;
+            
+            manageStep(steps, tabs,tabIndex);
+        })
+
+        prev.addEventListener('click', () => {
+            if(tabIndex == 0) return;
+
+            tabIndex -= 1;
+
+            manageStep(steps, tabs,tabIndex);
+        })
+    })
+
+    function manageStep(elements, tabs, active) {
+        activeWorkflow = elements[active];
+        activeTab = tabs[active];
+
+        tabs.forEach(element => {
+            if(element != active) element.classList.remove('active');
+        });
+
+        inactiveWorkflows = elements.filter((workflow, index) => index != active);
+        hideElements(inactiveWorkflows);
+
+        activeWorkflow.style.display = "block";
+        activeTab.classList.add('active');
+    }
+
+    function hideElements(elements, reverse = false) {
+        elements.forEach(element => {
+            element.style.display = 'none';
+        });
+    }
+</script>
