195b2dccff21a54da8d96f8fe6a9d0fa4647d3f8
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-22 11:38:52 -0600

    refactor: Implements error function to show errors.

--- views/Auth/confirmAccount.php
+++ views/Auth/confirmAccount.php
@@ -1,5 +1,7 @@
 <h1>Hola, estás a un solo paso más de poder utilizar tu cuenta.</h1>
-<p class="error alert"><?php echo error('token') ?></p>
+<?php if(error('token') != null) :?>
+    <p class="error alert"><?php echo error('token') ?></p>
+<?php endif ?>
 <p>Pulsa en el botón de abajo para confirmar tu cuenta</p>
 <form action="/confirm-account" method="post">
     <input type="hidden" name="token" value="<?php echo $token ?>">
