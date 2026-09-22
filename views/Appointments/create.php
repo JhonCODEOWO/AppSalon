a424b48751e11c19fdd38a4c7f0deedc9be0f079
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-22 15:42:14 -0600

    refactor: Implements necessary html to reutilize steps.js logic.

--- views/Appointments/create.php
+++ views/Appointments/create.php
@@ -15,13 +15,13 @@
         </button>
     </nav>
 
-    <div id="step-1">
+    <div class="step">
         <h2>Servicios</h2>
         <p>Elige los servicios deseados.</p>
         <div id="services"></div>
     </div>
     
-    <div id="step-2">
+    <div class="step">
         <h2>Tus datos y cita.</h2>
 
         <form action="">
@@ -59,7 +59,7 @@
         </form>
     </div>
 
-    <div id="step-3">
+    <div class="step">
         <h2>Resumen.</h2>
         <p>Verifica tus datos y finaliza.</p>
     </div>
@@ -73,59 +73,3 @@
         </button>
     </nav>
 </div>
-
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        //Get every HTML element necessary
-        const step1 = document.querySelector('#step-1');
-        const step2 = document.querySelector('#step-2');
-        const step3 = document.querySelector('#step-3');
-        const tabs = document.querySelectorAll('.tab');
-        const next = document.querySelector('#next');
-        const prev = document.querySelector('#previous');
-
-        //Global variables to manage
-        const steps = [step1, step2, step3];
-        const totalSteps = tabs.length-1;
-        let tabIndex = 0;
-
-        //Main method to manage every step and styles...
-        manageStep(steps, tabs,tabIndex);
-
-        next.addEventListener('click', () => {
-            if(tabIndex == totalSteps) return;
-            tabIndex += 1;
-            
-            manageStep(steps, tabs,tabIndex);
-        })
-
-        prev.addEventListener('click', () => {
-            if(tabIndex == 0) return;
-
-            tabIndex -= 1;
-
-            manageStep(steps, tabs,tabIndex);
-        })
-    })
-
-    function manageStep(elements, tabs, active) {
-        activeWorkflow = elements[active];
-        activeTab = tabs[active];
-
-        tabs.forEach(element => {
-            if(element != active) element.classList.remove('active');
-        });
-
-        inactiveWorkflows = elements.filter((workflow, index) => index != active);
-        hideElements(inactiveWorkflows);
-
-        activeWorkflow.style.display = "block";
-        activeTab.classList.add('active');
-    }
-
-    function hideElements(elements, reverse = false) {
-        elements.forEach(element => {
-            element.style.display = 'none';
-        });
-    }
-</script>
