a3a842fa1e136c0601f21f213d142488a03377c6
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-22 13:51:48 -0600

    feat: Adds public layout.

---
+++ views/layouts/public.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/build/main.css">
+</head>
+<body class="h-dvh flex flex-col">
+    <header class="p-3">
+        <h3>BarberShop</h3>
+    </header>
+    <div class="content flex-1">
+        <?php echo $content ?>
+    </div>
+</body>
+</html>
