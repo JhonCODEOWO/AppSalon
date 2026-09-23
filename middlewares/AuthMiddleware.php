be671a84e12cdf1ee8ff97c40c9cd981fcb90831
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-23 13:00:24 -0600

    refactor: Adds a error message to flash if AuthMiddleware can't continue.

--- middlewares/AuthMiddleware.php
+++ middlewares/AuthMiddleware.php
@@ -5,12 +5,16 @@ namespace Middlewares;
 use Closure;
 use Core\Auth;
 use Core\Interfaces\MiddlewareInterface;
+use Core\Session;
 use Routes\Request;
 
 class AuthMiddleware implements MiddlewareInterface{
     public function handle(Request $req, Closure $next) : mixed
     {
-        if(!Auth::authenticated()) redirectTo('/login');
+        if(!Auth::authenticated()) {
+            Session::flash("error", "You should be login to access.");
+            redirectTo('/login');
+        }
         return $next($req);
     }
 }
